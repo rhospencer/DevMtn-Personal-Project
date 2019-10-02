4f3e438dca4d9f4dbd8686c602024084f7b6d45d
Spencer Rhoton <rhotonspencer@gmail.com>
2019-10-02 17:33:19 -0600

    started adding joins to users table in get routes queries

--- src/Components/MyRoutes/MyRoutes.jsx
+++ src/Components/MyRoutes/MyRoutes.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
 export default class MyRoutes extends Component {
     constructor() {
@@ -8,16 +9,47 @@ export default class MyRoutes extends Component {
             city: '',
             distance: '',
             routes: [],
-            user_routes: false
+            userRoutes: false
         }
     }
 
+    componentDidMount() {
+        this.getPosts()
+    }
+
+    handleChange(e, key) {
+        this.setState({[key]: e.target.value})
+    }
+
+    handleCheckBoxChange() {
+        this.setState({userRoutes: !this.state.userRoutes})
+    }
+
+    getPosts() {
+        axios.get(`/api/saved_routes?user_routes=${JSON.stringify(this.state.userRoutes)}&city=${this.state.city}&distance=${this.state.distance}`).then(res => {
+            this.setState({routes: res.data})
+        })
+    }
 
 
     render() {
+        const route = this.state.routes.map(el => {
+            return <div className="route" key={el.route_id}>
+                <h1>{el.title}</h1>
+                <h4>{el.distance}</h4>
+                <img src={el.route_img} alt="Route Map"/>
+            </div>
+        })
         return(
             <div className="my-routes">
-                My Routes
+                <input onChange={e => this.handleChange(e, 'city')} placeholder="City" type="text"/>
+                <input value={this.state.distance} onChange={e => this.handleChange(e, 'distance')} placeholder="Distance" type="number"/>
+                <input value={this.state.distance} onChange={e => this.handleChange(e, 'distance')} placeholder="Distance" type="range" min="0" max="50"/>
+                <input onChange={() => this.handleCheckBoxChange()} checked={this.state.userRoutes} type="checkbox"/>
+                <button onClick={() => this.getPosts()}>Get Routes!</button>
+                <div className="route-holder">
+                    {route}
+                </div>
             </div>
         )
     }
